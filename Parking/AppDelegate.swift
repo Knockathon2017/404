99e14455188b71b59d223bd705b3ef38497c438a
Gaurav <gaurav@gaurav-bathinda.local>
2017-08-19 01:02:08 +0530

    AppDelegate

--- Parking/AppDelegate.swift
+++ Parking/AppDelegate.swift
@@ -49,6 +49,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     //Read JSON files
+    private func fetchParking() {
+        do {
+
+            if let file = Bundle.main.url(forResource: "Parking", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    // json is a dictionary
+                
+                    
+                } else if let object = json as? [Any] {
+                    // json is an array
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("no file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     private func readJson() {
         do {
             self.polygonArray = []
